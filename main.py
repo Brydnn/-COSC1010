181b3afdec69346ea7e11d7c5a122e943ff0b62f
Brydnn <brydnnkillpack@westernwyoming.edu>
2024-09-08 14:31:14 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -7,4 +7,9 @@
 # This is my first Python program.
 
 
-print("Hello World!")
+total_sales = float(input('Enter the projected sales:'))
+
+
+profit = total_sales * 0.23
+
+print('The profit is $', format(profit, ',.2f'))
