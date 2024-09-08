c8e5594504380cf68e1028d9959fa3109240dec2
Brydnn <brydnnkillpack@westernwyoming.edu>
2024-09-08 14:36:51 -0600

    Add files via upload

---
+++ Sales-Prodiction/main.py
@@ -0,0 +1,15 @@
+#
+# Name: Brydnn Killpack
+# Date: 8/29/2024
+# Your First Python Program
+# COSC 1010 
+
+# This is my first Python program.
+
+
+total_sales = float(input('Enter the projected sales:'))
+
+
+profit = total_sales * 0.23
+
+print('The profit is $', format(profit, ',.2f'))
