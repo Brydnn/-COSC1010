09509fe7b6e2e8fcbdac8fd1a4646121ad56e162
Brydnn <brydnnkillpack@westernwyoming.edu>
2024-09-11 18:55:53 -0600

    Update main.py

--- Sales-Tax/main.py
+++ Sales-Tax/main.py
@@ -1,4 +1,25 @@
 
+# Name Brydnn Killpack
+# Date 9/11/2024
+# Sales Tax Programming Project
+# COSC 2409 DNT
+#
+
+# Variable declarations
+
+# Constants for the state and county tax rates
+
+# Get the amount of the purchase.
+
+# Calculate the state sales tax.
+
+# Calculate the county sales tax.
+
+# Calculate the total tax.
+
+# Calculate the total of the sale.
+
+# Print information about the sale.
 total_sales = float(input('Enter the projected sales:'))
 
 
