56e374123d91f11fb7555cdaee72bb6ad92e8204
Brydnn <brydnnkillpack@westernwyoming.edu>
2024-09-11 21:05:03 -0600

    Update main.py

--- Sales-Tax/main.py
+++ Sales-Tax/main.py
@@ -8,19 +8,26 @@
 # Variable declarations
 
 # Constants for the state and county tax rates
+  
 state_tax_rate = .05
+
 county_tax_rate = .025
 
 # Get the amount of the purchase.
 purchase_amount = float(input("Enter purchase amount: $"))
+
 # Calculate the state sales tax.
 state_sales_tax = purchase_amount * state_tax_rate
+
 # Calculate the county sales tax.
 county_sales_tax = purchase_amount * county_tax_rate
+
 # Calculate the total tax.
 total_sales_tax = state_sales_tax + county_sales_tax
+
 # Calculate the total of the sale.
 total_sale_amount = purchase_amount + total_sales_tax
+
 # Print information about the sale.
 print(f"Purchase Amount: ${purchase_amount:.2f}")
 print(f"State Sales Tax: ${state_sales_tax:.2f}")
