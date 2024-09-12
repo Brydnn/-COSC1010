03e3387618c06856b79ad50beb25cdd6b2603e67
Brydnn <brydnnkillpack@westernwyoming.edu>
2024-09-11 20:57:55 -0600

    Update main.py

--- Sales-Prodiction/main.py
+++ Sales-Prodiction/main.py
@@ -8,15 +8,8 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
-# Calculate the projected profit.
-
-# Print the projected profit.
-
-
 total_sales = float(input('Enter the projected sales:'))
-
-
+# Calculate the projected profit.
 profit = total_sales * 0.23
-
+# Print the projected profit.
 print('The profit is $', format(profit, ',.2f'))
